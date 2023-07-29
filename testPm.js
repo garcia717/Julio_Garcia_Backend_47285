eae8c4496bc45a97077fe7785886fc8a6f5478d6
julio Garcia <garcia717@gmail.com>
2023-07-29 14:20:34 -0300

    "probando el codigo"

--- testPm.js
+++ testPm.js
@@ -3,7 +3,6 @@ const ProductManager = require("./productManager");
 const manager = new ProductManager();
 
 try {
-  console.log("getProducts() al principio:");
   console.log(manager.getProducts());
 
   manager.addProduct({
@@ -14,32 +13,24 @@ try {
     code: "abc123",
     stock: 25,
   });
-
-  console.log("\nProducto agregado satisfactoriamente:");
   console.log(manager.getProducts());
 
   try {
     manager.addProduct({
-      title: "producto repetido",
-      description: "Este producto ya existe",
-      price: 150,
-      thumbnail: "Sin imagen",
-      code: "abc123", 
-      stock: 10,
+        title: "producto prueba",
+        description: "Este es un producto prueba",
+        price: 200,
+        thumbnail: "Sin imagen",
+        code: "abc123",
+        stock: 25,
     });
   } catch (error) {
-    console.error(`Error al agregar producto: ${error.message}`);
+    console.error(` ${error.message}`);
   }
-
-  console.log("\nProducto después de intentar agregar duplicado:");
   console.log(manager.getProducts());
-
   const productById = manager.getProductById(1); 
-  console.log("\nProducto por ID:");
   console.log(productById);
-
   const nonExistentProduct = manager.getProductById(6);  
-  console.log("\nProducto inexistente:");
   console.log(nonExistentProduct);
 } catch (error) {
   console.error(error.message);
