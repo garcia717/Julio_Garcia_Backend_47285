c1671303767a0a4f3f7e3e861a998f1189840b68
julio Garcia <garcia717@gmail.com>
2023-08-02 21:05:43 -0300

    Agrego branch para segundo desafio

--- productManager.js
+++ productManager.js
@@ -1,55 +1,94 @@
+const fs = require("fs");
+
 class ProductManager {
-    constructor() {
-        this.products = [];
-        this.nextId = 1;
+  constructor(filePath) {
+    this.products = [];
+    this.nextId = 1;
+    this.path = filePath;
+  }
+
+  async addProduct(productData) {
+    const { title, description, price, thumbnail, code, stock } = productData;
+    if (!title || !description || !price || !thumbnail || !code || !stock) {
+      throw new Error("Todos los campos son obligatorios.");
     }
 
-    addProduct(productData) {
-        const {
-            title,
-            description,
-            price,
-            thumbnail,
-            code,
-            stock
-        } = productData;
-        if (!title || !description || !price || !thumbnail || !code || !stock) {
-            throw new Error("Todos los campos son obligatorios.");
-        }
-
-        const existingProduct = this.products.find((product) => product.code === code);
-        if (existingProduct) {
-            throw new Error(`El código '${code}' ya está asignado a otro producto.`);
-        }
-
-        const newProduct = {
-            id: this.nextId,
-            title,
-            description,
-            price,
-            thumbnail,
-            code,
-            stock,
-        };
-
-        this.products.push(newProduct);
-        this.nextId++;
+    const existingProduct = this.products.find((product) => product.code === code);
+    if (existingProduct) {
+      throw new Error(`El código '${code}' ya está asignado a otro producto.`);
     }
 
-    getProducts() {
-        return this.products;
+    const newProduct = {
+      id: this.nextId,
+      title,
+      description,
+      price,
+      thumbnail,
+      code,
+      stock,
+    };
+
+    this.products.push(newProduct);
+    this.nextId++;
+
+    await this.saveToFile();
+  }
+
+  async getProducts() {
+    await this.loadFromFile();
+    return this.products;
+  }
+
+  getProductById(id) {
+    const product = this.products.find((product) => product.id === id);
+    if (!product) {
+      console.error("Not found");
+    }
+    return product;
+  }
+
+  async updateProduct(id, updatedFields) {
+    const productIndex = this.products.findIndex((product) => product.id === id);
+    if (productIndex === -1) {
+      throw new Error("Producto no encontrado");
     }
 
-    getProductById(id) {
-        const product = this.products.find((product) => product.id === id);
-        if (!product) {
-            console.error("Not found");
-        }
-        return product;
+    this.products[productIndex] = {
+      ...this.products[productIndex],
+      ...updatedFields,
+      id,
+    };
+
+    await this.saveToFile();
+  }
+
+  async deleteProduct(id) {
+    const updatedProducts = this.products.filter((product) => product.id !== id);
+    if (updatedProducts.length === this.products.length) {
+      throw new Error("Producto no encontrado");
     }
+
+    this.products = updatedProducts;
+    await this.saveToFile();
+  }
+
+  async loadFromFile() {
+    try {
+      const data = await fs.promises.readFile(this.path, "utf8");
+      this.products = JSON.parse(data);
+      this.nextId = this.products.reduce((maxId, product) => Math.max(maxId, product.id), 0) + 1;
+    } catch (error) {
+      this.products = [];
+    }
+  }
+
+  async saveToFile() {
+    try {
+      await fs.promises.writeFile(this.path, JSON.stringify(this.products, null, 2), "utf8");
+    } catch (error) {
+      console.error("Error al guardar los productos:", error);
+    }
+  }
 }
 
-
 module.exports = ProductManager;
-
-
--- testPm.js
+++ testPm.js
@@ -1,37 +1,64 @@
 const ProductManager = require("./productManager");
 
-const manager = new ProductManager();
+function test() {
+  const manager = new ProductManager("./products.json");
 
-try {
-  console.log(manager.getProducts());
-
-  manager.addProduct({
-    title: "producto prueba",
-    description: "Este es un producto prueba",
-    price: 200,
-    thumbnail: "Sin imagen",
-    code: "abc123",
-    stock: 25,
-  });
+  console.log("getProducts() al principio:");
   console.log(manager.getProducts());
 
   try {
     manager.addProduct({
-        title: "producto prueba",
-        description: "Este es un producto prueba",
-        price: 200,
-        thumbnail: "Sin imagen",
-        code: "abc123",
-        stock: 25,
+      title: "producto prueba",
+      description: "Este es un producto prueba",
+      price: 200,
+      thumbnail: "Sin imagen",
+      code: "abc123",
+      stock: 25,
     });
+
+    console.log("\nProducto agregado satisfactoriamente:");
+    console.log(manager.getProducts());
+
+    const productById = manager.getProductById(1); // Suponiendo que el id generado es 1
+    console.log("\nProducto por ID:");
+    console.log(productById);
+
+    // Intentamos agregar el mismo producto con el mismo código
+    try {
+      manager.addProduct({
+        title: "producto repetido",
+        description: "Este producto ya existe",
+        price: 150,
+        thumbnail: "Sin imagen",
+        code: "abc123", // Código repetido
+        stock: 10,
+      });
+    } catch (error) {
+      console.error(`Error al agregar producto: ${error.message}`);
+    }
+
+    console.log("\nProducto después de intentar agregar duplicado:");
+    console.log(manager.getProducts());
+
+    manager.updateProduct(1, {
+      price: 250
+    });
+    console.log("\nProducto después de actualizar:");
+    console.log(manager.getProducts());
+
+    try {
+      manager.deleteProduct(1);
+      console.log("\nProducto después de eliminar:");
+      console.log(manager.getProducts());
+    } catch (error) {
+      console.error(`Error al eliminar producto: ${error.message}`);
+    }
+
+    console.log("\nProducto después de intentar eliminar de nuevo:");
+    console.log(manager.getProducts());
   } catch (error) {
-    console.error(` ${error.message}`);
+    console.error(error.message);
   }
-  console.log(manager.getProducts());
-  const productById = manager.getProductById(1); 
-  console.log(productById);
-  const nonExistentProduct = manager.getProductById(6);  
-  console.log(nonExistentProduct);
-} catch (error) {
-  console.error(error.message);
 }
+
+test();
