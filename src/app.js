97b9e823ff32fe7b018abc3ca0b3c625630f8e1a
julio Garcia <garcia717@gmail.com>
2023-08-10 20:24:43 -0300

    Desafio corregido

--- src/app.js
+++ src/app.js
@@ -1,29 +1,52 @@
-import express from 'express'
-import { readFile } from 'fs/promises';
+import express from 'express';
+import {
+    readFile
+} from 'fs/promises';
 
+const PORT = 8080;
+const app = express();
 
-const PORT = 8080
-const app = express()
-const productsData = await readFile('products.json', 'utf-8');
-const products = JSON.parse(productsData);
-app.use(express.json())
+app.use(express.json());
 
+(async () => {
+    const productsData = await readFile('products.json', 'utf-8');
+    const products = JSON.parse(productsData);
 
-app.get('/', (req, res) => {
-    res.send('Hola')
-})
+    app.get('/', (req, res) => {
+        res.send('Hola');
+    });
 
-app.get('/products', (req, res) => {
-    const {category} = req.query
-    if (category) {
-        const prods = products.filter(prod => prod.category === category)
-        res.status(200).send(prods)
-    }
-    res.status(200).send(products)
-})
+    app.get('/products', (req, res) => {
+        const {
+            category,
+            limit
+        } = req.query;
+        let prods = [...products];
 
+        if (category) {
+            prods = prods.filter(prod => prod.category === category);
+        }
 
+        if (limit) {
+            const limitValue = parseInt(limit);
+            if (!isNaN(limitValue) && limitValue > 0) {
+                prods = prods.slice(0, limitValue);
+            }
+        }
 
-app.listen(PORT, () => {
-    console.log(`Server on port ${PORT}`)
-})
+        res.status(200).send(prods); 
+    });
+
+    app.get('/products/:id', (req, res) => {
+        const prod = products.find(prod => prod.id === parseInt(req.params.id));
+        if (prod) {
+            res.status(200).send(prod);
+        } else {
+            res.status(404).send("Producto no existente");
+        }
+    });
+
+    app.listen(PORT, () => {
+        console.log(`Server on port ${PORT}`);
+    });
+})();
